bfc0100d43c796dba0e2c5ad094c980fc95d6941
Raul Ramirez Perez <ramssrez@gmail.com>
2021-09-10 15:50:01 -0500

    Agregación de los archivos de JS que se han realizado en la semana 2

---
+++ EjerciciosBasicosJS/JS/ciclos.js
@@ -0,0 +1,6 @@
+//alert("Hola en ciclos");
+var frutas = ["Mango", "Piña", "Naranja"];
+for(var i = 0; i < frutas.length ; i++){
+    console.log("Fruta " + i + ": " +frutas[i]);
+}
+//esto es una prueba 
