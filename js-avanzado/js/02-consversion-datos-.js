a0ae070032a3b37d1b443c980172852e7b236b90
Raul Ramirez Perez <ramssrez@gmail.com>
2021-09-21 12:07:37 -0500

    Creación y uso de conversion de datos

---
+++ js-avanzado/js/02-consversion-datos-.js
@@ -0,0 +1,8 @@
+const nombre = prompt("¿Cual es tu nombre?");
+//Parseo de un String a un number con parseInt()
+const edad =  parseInt(prompt("¿Cual es tu edad?"));
+//Convertir un String a un number
+console.log(typeof(nombre));
+console.log(typeof(edad));
+//Cambiar de un number a String
+console.log(typeof(edad.toString()));
