bb7eaa7c4b4fe8fbafc563c5543365c8eef37a82
Raul Ramirez Perez <ramssrez@gmail.com>
2021-09-21 11:56:34 -0500

    Uso de  para ingresar string dentro de un texto

--- js-avanzado/js/01-template-string.js
+++ js-avanzado/js/01-template-string.js
@@ -1,1 +1,8 @@
-alert("Template Js")
+const nombre = prompt("¿Cual es tu nombre?");
+const edad = prompt("¿Cual es tu edad?");
+console.log("Hola " + nombre + " saludos. Tu edad es: " + edad + " años");
+//Uso de comillas `` (alt + 96 ASCII) para que se ingrese un mensaje con las constantes ingresadas 
+const saludo = `Hola ${nombre} saludos. Tu edad es: ${edad} años`;
+console.log("Esto es con estas comillas `` ")
+console.log(saludo);
+document.write(saludo);
