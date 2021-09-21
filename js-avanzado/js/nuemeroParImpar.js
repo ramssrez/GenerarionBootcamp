99a65bbd0568347197a876d16ff75bb7c50c93f3
Raul Ramirez Perez <ramssrez@gmail.com>
2021-09-21 13:27:20 -0500

    Modificación de la función del ejercicio

--- js-avanzado/js/nuemeroParImpar.js
+++ js-avanzado/js/nuemeroParImpar.js
@@ -1,16 +1,11 @@
 //Variable que declara el año de nacimiento del usuario
 var numero = parseInt(prompt("¿Ingresa el numero que quieres conocer"));
-if(isPar(numero)){
-    document.write(`El número  ${numero} es Par`);
-}else{
-    document.write(`El número  ${numero} es Impar`);
-}
-
+isPar(numero);
 function isPar(value){
     if(value%2 === 0){
-        return true;
+        document.write(`El número  ${value} es Par`);
     }else{
-        return false;
+        document.write(`El número  ${value} es Impar`);
     }
 }
 
