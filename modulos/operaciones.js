7e4f0860f9ceb9b70ecb7bc3d8c32a68403e97b6
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-04 11:48:57 -0500

    uso de modulos con JS

---
+++ modulos/operaciones.js
@@ -0,0 +1,10 @@
+export const suma = (num1,num2) =>{
+    return num1 + num2;
+}
+const resta = (num1,num2) =>{
+    return num1 - num2;
+}
+const multiplicacion = (num1,num2) =>{
+    return num1 * num2;
+}
+export const saludo = "Esto es un saludo de Raúl Ramírez desde un modulo";
