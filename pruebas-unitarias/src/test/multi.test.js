a5c074a75f569130d0df10f0d4a0d660587c63dd
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-05 11:49:58 -0500

    Creación de archivos para las pruebas con multiplicación y resta

--- pruebas-unitarias/src/test/multi.test.js
+++ pruebas-unitarias/src/test/multi.test.js
@@ -1,6 +1,6 @@
-import { suma } from '../js/suma';
+import { multi } from '../js/multi';
 test('adds 1 + 2 to equal 3', ()=>{
-    expect(suma(1, 2)).toBe(3);
+    expect(multi(4, 2)).toBe(8);
 });
 /* Comando utilizado
      node --experimental-vm-modules node_modules/jest/bin/jest.js
--- pruebas-unitarias/src/test/resta.test.js
+++ pruebas-unitarias/src/test/resta.test.js
@@ -1,6 +1,6 @@
-import { suma } from '../js/suma';
+import { resta } from '../js/resta';
 test('adds 1 + 2 to equal 3', ()=>{
-    expect(suma(1, 2)).toBe(3);
+    expect(resta(5, 2)).toBe(3);
 });
 /* Comando utilizado
      node --experimental-vm-modules node_modules/jest/bin/jest.js
