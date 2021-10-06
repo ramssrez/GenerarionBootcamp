d51f6f6848a8ef55e3e3070b26d1fe8c83df769b
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-06 17:09:43 -0500

    Transformación del codigo a lenguaje maquina

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hola mundo desde Java"); 
+        System.out.print("Hola mundo desde Java");
+        //Uso de javac "Nombre archivo para compilarlo a lenguaje codigo" 
     }
 }
