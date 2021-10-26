2bc87cfcb7232c65128e1ad6ba03a7918a42b079
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-26 15:47:06 -0500

    Creación de las sentencias para insertar

---
+++ basedatos/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO usuarios VALUES (NULL, "Federico", "fedelobo@gmail.com","essecreta","1025 estaciom 3/4");
+INSERT INTO usuarios VALUES (NULL, "Dobby","soylibre@gmail.com","libre soty","Calderos de Howarts");
+INSERT INTO usuarios VALUES (NULL, "Frodo","elprecioso@gmail.com","soypunk","Ecatepec");
