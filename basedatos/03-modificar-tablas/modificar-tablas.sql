97e9687cd6e58be28bfb35607a7515f1b6c9e238
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-26 15:33:19 -0500

    Uso del alter para modificar cambios de la tabla

--- basedatos/03-modificar-tablas/modificar-tablas.sql
+++ basedatos/03-modificar-tablas/modificar-tablas.sql
@@ -1,1 +1,11 @@
+/* Renombrar tabla */
 ALTER TABLE usuarios RENAME TO users;
+
+/* Cambiar el nombre de una columna */
+ALTER TABLE users CHANGE direccion dir VARCHAR(50);
+
+/* Agregar columnas */
+ALTER TABLE usuarios ADD edad INT NOT NULL;
+
+/*Borrar columna */
+ALTER TABLE usuarios DROP edad;
