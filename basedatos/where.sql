5bd5fb9c326c593a3632183bd2148ece79a8ba99
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-27 10:31:46 -0500

    Uso de las sentencias para encontrar información sobre ids

--- basedatos/where.sql
+++ basedatos/where.sql
@@ -1,1 +1,20 @@
-SELECT * FROM tablas WHERE id = 5;
+SELECT * FROM actor WHERE actor_id = 135;
+/* Uso de operadores logicos */
+/*
+OPERADORES DE COMPARACION:
+Igual           =
+Distinto        !=
+Menor           <
+Mayor           >
+Menor o igual   <=
+Mayor o igual   >=
+Entre           between A and B
+En              in
+Es nulo         is null
+No nulo         is not null
+Como            like
+No es como      not like
+*/
+/* Seleccionar por id mayor a un numero */
+SELECT * FROM actor WHERE actor_id > 63;
+SELECT * FROM actor WHERE actor_id >= 63;
