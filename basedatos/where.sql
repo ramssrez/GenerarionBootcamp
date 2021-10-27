4850c3a9a821ea491d89ef185fac3018afc76ff3
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-27 10:47:26 -0500

    Uso de operadores

--- basedatos/where.sql
+++ basedatos/where.sql
@@ -18,3 +18,19 @@ No es como      not like
 /* Seleccionar por id mayor a un numero */
 SELECT * FROM actor WHERE actor_id > 63;
 SELECT * FROM actor WHERE actor_id >= 63;
+
+/* Seleccionar campos */
+SELECT actor_id, first_name, last_name FROM actor WHERE actor_id > 63;
+
+/*Selecionar elementos con id pares  */
+SELECT*FROM actor WHERE actor_id%2=0;
+
+/*
+OPERADORES LOGICOS:
+O       OR
+Y       AND
+NO      NOT
+*/
+
+/* Operador AND */
+SELECT* FROM actor WHERE first_name = "CHRIS" AND actor_id > 50;
