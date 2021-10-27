ee653a8a116c78f08f7a34cace3588fd63f0df59
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-27 15:02:28 -0500

    Multiplicacin de tablas

---
+++ basedatos/07-multit-ablas/multitablas.sql
@@ -0,0 +1,6 @@
+/* JOINS */
+/* Consulta la información de dos tablas que no se relacionan*/
+SELECT * FROM actor, film_text;
+
+/* Consulta de información de dos tablas que se relacionan */
+SELECT * FROM actor, film_actor;
