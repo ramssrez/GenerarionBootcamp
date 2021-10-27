ff894f5212f4150adffe6c8583b6e3f8f42f5e86
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-27 12:42:15 -0500

    Uso del between y consultas y sobre consultas

--- basedatos/06-subconsultas/subconsultas.sql
+++ basedatos/06-subconsultas/subconsultas.sql
@@ -1,1 +1,10 @@
-SELECT * FROM category;
+/*Consultas sobre otras consultas*/
+
+/*Uso de between para un rango de elementos*/
+SELECT * FROM film WHERE film_id BETWEEN 1 AND 10;
+
+/* Trae varias varios id de la información */
+SELECT * FROM film WHERE film_id IN (1,2,3);
+
+/* Obtiene la información de las dos tablas */
+SELECT * FROM film WHERE film_id IN (SELECT film_id FROM film_category WHERE category_id = 5);
